9235b20a3dfa5fede41b3739db209ca15002cd83
Gabriel Candal <gabrielcandal@gmail.com>
2014-03-05 16:52:49 +0000

    Added input file parser.

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
 package neuralNetwork;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(new NeuralNetwork(1, 3, 2));
+		Parser parser = null;
+		
+		try {
+			parser = new Parser("diagnosis.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't open file");
+			
+			return;
+		}
+		ArrayList<ArrayList<Double>> inputs = null;
+		
+		try {
+			inputs = parser.parseFile();
+		} catch (IOException e) {
+			System.out.println("There was a problem reading the file");
+			
+			return;
+		}
+
+		for(ArrayList<Double> inputSet: inputs)
+			System.out.println(inputSet);
+		
+		//System.out.println(new NeuralNetwork(1, 3, 2));
 	}
 }
---
+++ Parser.java
@@ -0,0 +1,49 @@
+package neuralNetwork;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Parser {
+	private BufferedReader file;
+	
+	public Parser(String filename) throws FileNotFoundException {
+		file = new BufferedReader(new FileReader(filename));
+	}
+	
+	public ArrayList<ArrayList<Double>> parseFile() throws IOException {
+		ArrayList<ArrayList<Double>> result = new ArrayList<ArrayList<Double>>();
+		String line = "";
+		
+		while(true) {
+			line = file.readLine();
+			
+			if(line == null || line.isEmpty())
+				return result;
+			
+			result.add(parseLine(line));
+		}
+	}
+	
+	private ArrayList<Double> parseLine(String line) {
+		ArrayList<Double> result = new ArrayList<Double>();
+		
+		for(String word: line.split("\t"))
+			try {
+				
+				result.add(Double.parseDouble(word.replace(",", ".")));
+				
+			} catch(NumberFormatException e) {
+				
+				if(word.equals("yes"))
+					result.add(1.0);
+				else
+					result.add(0.0);
+				
+			}
+		
+		return result;
+	}
+}
